94fcdb69ea01a4e031b093cf781d047810245caf
AbelWondimuL <abelwondimu208@gmail.com>
2024-11-12 13:02:16 -0800

    Add initial implementation of QuestionScreen widget

---
+++ lib/screens/questionScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Questionscreen extends StatefulWidget {
+  const Questionscreen({super.key});
+
+  @override
+  State<Questionscreen> createState() => _QuestionscreenState();
+}
+
+class _QuestionscreenState extends State<Questionscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Question Screen');
+  }
+}
